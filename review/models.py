bd53760a508eb019e96343fb7692bcb6f1598b0e
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-09-20 13:07:44 +0300

    initial migrations

--- review/models.py
+++ review/models.py
@@ -1,8 +1,60 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import BaseUserManager,AbstractBaseUser
+
 # Create your models here.
-c
+class MyAccountManager(BaseUserManager):
+    def create_user(self,email,username,password=None):
+        if not email:
+            raise ValueError("User must have an email address")
+        if not username:
+            raise ValueError("User must have a username")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self,email,username,password):
+        user = self.self.create_user(
+            email=self.normalize_email(email),
+            password=password,
+            username=username
+        )
+        user.email = email
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class Users(AbstractBaseUser):
+    username = models.CharField( max_length=20, unique=True)  
+    email = models.CharField( max_length=50, unique=True)
+    name = models.CharField( max_length=50, unique=False)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    password = models.CharField( max_length=100)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username','password']
+
+    objects=MyAccountManager()
+     
+    def _str_(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
+
 
 class Profile(models.Model):
     user = models.OneToOneField('Users',on_delete = models.CASCADE)
@@ -11,7 +63,14 @@ class Profile(models.Model):
     photo = CloudinaryField('prof',null=True,blank=True)
     signup_confirmation = models.BooleanField(default=False)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username','password']
-
+    def __str__(self) ->str:
+        return f'{self.user.username}'
     
+    def save_profile(self):
+        self.user.save()
+
+    @classmethod
+    def search_profile(cls,uname):
+        return cls.objects.filter(user__username__icontains=uname).all()
+
+
