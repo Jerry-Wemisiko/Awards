489f7f42c8074fd0ddb33b588c67b2f04bbbc2b8
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-09-20 14:01:16 +0300

    new signup view

--- review/views.py
+++ review/views.py
@@ -1,5 +1,6 @@
-from review.models import Profile
+from review.models import Profile,Project,Users
 from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
 def homepage(request):
@@ -8,3 +9,15 @@ def homepage(request):
     projects = Project.objects.all()
 
     return render(request,'index.html',{'title':title,'profile':profile,'projects':projects})
+
+def SignUp(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        if password == confirm_password:
+            user= Users(username=username, email =email,password=make_password(password))
+        else:
+            return('Passwords do not match')
+    return render(request,'auth/signup.html')
