d7408aac327ef4df34683060d7a27f4d715ac4e1
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-09-27 14:51:47 +0300

    fixed a db error

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from django.db import models
+from review.models import Profile,Project,Review
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(Review)
